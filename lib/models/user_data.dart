80cb65b4116c2c77babd154f3aa6a57d33c103fb
gyvm <jolcde75+github@gmail.com>
2021-03-30 19:55:43 +0900

    add provider class

---
+++ lib/models/user_data.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+
+class UserDataModel extends ChangeNotifier {
+  // final List<String> _checkInterval = [
+  //   '1 day',
+  //   '3 day',
+  //   '5 day',
+  //   '1 week',
+  //   '2 week',
+  //   'a month'
+  // ];
+
+  // final List<String> _retryCount = ['1 times', '3 times'];
+
+  // Map<String, dynamic> _config = {
+  //   'last_check': 'Timestamp(seconds=1614339150, nanoseconds=976890000)',
+  //   'next_check': 'Timestamp(seconds=1614339150, nanoseconds=976890000)',
+  //   'name': 'ok',
+  //   'email': '',
+  //   'phone_number': 'ok',
+  //   'check_interval': '2 week',
+  //   'retry_count': '3 times',
+  // };
+
+  // // Map<String, dynamic> contacts = {
+  // //   'last_check': 'Timestamp(seconds=1614339150, nanoseconds=976890000)',
+  // //   'next_check': 'Timestamp(seconds=1614339150, nanoseconds=976890000)',
+  // //   'name': 'ok',
+  // //   'email': '',
+  // //   'phone_number': 'ok',
+  // //   'check_interval': '2 week',
+  // //   'retry_count': '3 times',
+  // // };
+
+  // List<String> get checkInterval => _checkInterval;
+
+  // List<String> get retryCount => _retryCount;
+
+  // Map<String, dynamic> get config => _config;
+
+  // int _count = 0;
+
+  // int get count => _count;
+
+  // void increment() {
+  //   _count++;
+  //   print(_count.toString());
+  //   notifyListeners();
+  // }
+
+  // void update(String key, dynamic data) {
+  //   _config[key] = data;
+  //   notifyListeners();
+  //   // just print for dev.
+  //   print("in config : " + _config[key].toString());
+  //   print("success update!");
+  // }
+
+  // // contactsのwidget用に登録されている連絡先の名前2文字とステータスを返す。
+  // // void getContacts() {
+  // //   print("in config : " + _config[key].toString());
+  // //   print("success update!");
+  // // }
+}
